ba277dc634824a8c5410c0bade316ebe012036b5
NathanProDocs <70610611+NathanProDocs@users.noreply.github.com>
2021-01-22 22:24:12 -0500

    Add files via upload
    Nim implementation of the Sithole algorithm as described in Sithole (2001).  Script iterates through millions of points in a LiDAR point cloud assessing each point and its neighboring points with a slope-based morphological filter.  Works well to remove vegetation as well as walls/fences and building facades.

--- GEOG6060_Classifier_Final.nim
+++ GEOG6060_Classifier_Final.nim
@@ -3,8 +3,12 @@ import strabo/lidar
 import kdtree
 import math
 import cligen
-import strutils
 
+##nim c -d:danger.\"program name"
+##.\"program name".exe getinfo
+
+#####Sithole algorithm based off a slope based filter proposed by Vosselman, (2000) and modified to include tophat morphological filter by Sithole, (2001).  The algorithm takes user input variables and filters any .las file based on slope.#####
+#####Nathan Adams, ID 0928773#####
 proc run(wd: string = "C:/Users/Natha/Data/", lidarFile: string = "1km_input.las", outputFile: string = "Demo.las", searchDist, minOffGroundPtHeight, maxDownwardSlope: float) = 
 
     let
@@ -12,10 +16,10 @@ proc run(wd: string = "C:/Users/Natha/Data/", lidarFile: string = "1km_input.las
     echo("Reading Points...")
 
     var
-        las = newLasFile(lidarFile) #Reading in the initial LiDAR point cloud
-        output = initializeUsingFile(outputFile, las) #Creates the new las file
-        points = newSeqOfCap[array[K, float]](las.header.numberOfPoints) #creates an array of points
-        values = newSeqOfCap[int](las.header.numberOfPoints) #creates an array of values for those points
+        las = newLasFile(lidarFile) 
+        output = initializeUsingFile(outputFile, las) 
+        points = newSeqOfCap[array[K, float]](las.header.numberOfPoints) 
+        values = newSeqOfCap[int](las.header.numberOfPoints) 
         pd, pdNeighbour: PointRecord3 #gets the point record data from each point
         progress: int 
         old_progress = 1 
@@ -29,8 +33,8 @@ proc run(wd: string = "C:/Users/Natha/Data/", lidarFile: string = "1km_input.las
         if pd.bitField.returnNumber() == pd.bitField.numberOfReturns():   # check if the return number equals the total number of returns (e.g. if it is the 5th return, was there 5 returns total)
             points.add([pd.x, pd.y])                         
             values.add(p)  
-            
-            offGroundPoint.add(false)                                   
+            offGroundPoint.add(false)   
+                                            
         else:
             offGroundPoint.add(true)                                      
 
@@ -46,6 +50,7 @@ proc run(wd: string = "C:/Users/Natha/Data/", lidarFile: string = "1km_input.las
 
     #Below is white tophat transform
     #Erosion
+
     var
         minElevation = newSeqOfCap[float](las.header.numberOfPoints)
         minimumZ: float
@@ -69,7 +74,6 @@ proc run(wd: string = "C:/Users/Natha/Data/", lidarFile: string = "1km_input.las
     var
         tophatz = newseqofcap[float](las.header.numberOfPoints)
         maximumZ: float
-        #nnDist = newseqofcap[float](las.header.numberOfPoints)
 
     for p in 0..<las.header.numberOfPoints.int:
         pd = las.getPointRecord(p)
@@ -78,16 +82,16 @@ proc run(wd: string = "C:/Users/Natha/Data/", lidarFile: string = "1km_input.las
             var ret = tree.withinRadius([pd.x, pd.y], radius=searchDistSqr, sortResults=false)
             for (_, n, dist) in ret:
                 if minElevation[n] > maximumZ:
-                    maximumZ = minElevation[n] #If the minElevation is greater than the hgihest elevation in a neighbourhood
+                    maximumZ = minElevation[n]      #If the minElevation is greater than the hgihest elevation in a neighbourhood
                                                     #maximumZ is increased to match that of minElevation
-        tophatz.add(pd.z - maximumZ) #adds values to an array representing the point of interest - highest elevation point in the neighbourhood
+        tophatz.add(pd.z - maximumZ) 
 
         progress = (100.0 * (p.float + 1.0) / las.header.numberOfPoints.float).int  
         if progress mod 5 == 0 and progress != oldProgress:
             oldProgress = progress
             echo(&"Dilation: {progress}%")
         
-    #3.	Visit each point in the LAS file, if the current value of the point index in 'offGroundPoint' is false then perform a withinRadius search at the site of the point's x,y coordinates. 
+    #Slope-based filter
     for p in 0..<las.header.numberOfPoints.int: 
         pd = las.getPointRecord(p)
         if pd.bitField.returnNumber() == pd.bitField.numberOfReturns():
@@ -95,41 +99,40 @@ proc run(wd: string = "C:/Users/Natha/Data/", lidarFile: string = "1km_input.las
             for (_, n, dist) in ret:                                                     
                 run = sqrt(dist)
                 pdNeighbour = las.getPointRecord(n)                                                                          
-                if pdNeighbour.classField.classification != lowPoint and pdNeighbour.classField.classification != highNoise: #loop through each point and discard those which are classed as lowpoints or with highnoise
-                    #4.	For each neighbouring point, measure the slope between the neighbour and the point of interest at the center of the search. 
-                    #The rise = difference in elevation between the neighbour and the point of interest, the run is the distance separating them. slope = arctan(rise/run)--remember to convert to degrees.
+                if pdNeighbour.classField.classification != lowPoint and pdNeighbour.classField.classification != highNoise: 
                     rise = tophatz[p] - tophatz[n]
                     slope = radToDeg(arctan(rise/run))
-                    #5.	Find the maximum DOWNWARD slope (i.e. if you measure rise as PointOfInterest elevation - neighbour elevation then you are looking for the maximum positive slope value...otherwise you want the maximum negative value). 
-                    #We essentially only want to know the slopes between the point of interest and neighbours that are lower than it.
                     if slope > maxDownwardSlope and rise >= minOffGroundPtHeight:
-                        #6.	If the maximum downward slope is above a user-specified threshold value (in degrees) then the point of interest is an off-ground point and you should mark it as true in the 'offGroundPoint' array.
-                        offGroundPoint.add(true) #if our point meets all of the above constraints it will be added back to our boolean array as a "true" value, signifying an offgroundpoint
+                        offGroundPoint[p] = true #if our point meets all of the above constraints it will be changed in the boolean array as a "true" value, signifying an offgroundpoint
+
         progress = (100.0 * (p.float + 1.0) / las.header.numberOfPoints.float).int 
         if progress mod 5 == 0 and progress != oldProgress:
             oldProgress = progress
             echo(&"Slope-based filter: {progress}%")
-
+        
+    #Output pointrecord
     for p in 0..<las.header.numberOfPoints.int: 
-        if offGroundPoint[p] == false:  #sq brackets for index, round for function
+        if offGroundPoint[p] == false:  
             pd = las.getPointRecord(p)
             output.addPointRecord(pd)
 
     output.write()
+    
 
+#Beginning of user-defined variables using Cligen#
 proc getinfo() = 
-    echo("/////////////////////////////////")
-    echo("/Sithole + White Tophat Filter/")
-    echo("/////////////////////////////////")
+    echo("/////////////////////////////////")                 #C:\Users\Natha\Data\GEOG6060Data
+    echo("//Sithole + White Tophat Filter//")                 #Guelph.las
+    echo("/////////////////////////////////")                 #GuelphDemo().las
 
-    write(stdout, "Working Directory: ") #   C:\Users\Natha\Data\
+    write(stdout, "Working Directory: ") 
     let wd = readLine(stdin)
 
-    write(stdout, "LiDAR Input File: ") #   1km_input.las
+    write(stdout, "LiDAR Input File: ") 
     let lidarFile = readLine(stdin)
     doAssert len(lidarFile) > 0, "Error input file name is empty."
 
-    write(stdout, "Output File: ") #   Demo.las
+    write(stdout, "Output File: ") 
     let outputFile = readLine(stdin)
     doAssert len(outputFile) > 0, "Error output file name is empty."
 
@@ -151,8 +154,9 @@ proc getinfo() =
     if len(maxDownwardSlopeUD) > 0:
         maxDownwardSlope = parsefloat(maxDownwardSlopeUD)
 
+    #Appending the file name to the working directory if entered correctly#
     var inFile = if wd.len() > 0:
-            wd & lidarFile #This part is required to join the input files and output files to the working directory, will throw an error if not added
+            wd & lidarFile 
         else:
             lidarFile
 
@@ -160,7 +164,7 @@ proc getinfo() =
             wd & outputFile
         else:
             outputFile
-
+    
     run(wd, inFile, outFile, searchDist, minOffGroundPtHeight, maxDownwardSlope)
 
 proc main() = 
@@ -174,5 +178,5 @@ proc main() =
         ],
         [getinfo]
         )
-echo("Done!")
 main()
+
